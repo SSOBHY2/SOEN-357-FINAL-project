fa20850b52bbd0db831b8dbd7bf7dec39798dbf6
SSOBHY2 <mohamedsobhy672005@gmail.com>
2025-11-17 21:02:26 -0500

    update config.js

--- lexiloop-extension/config.js
+++ lexiloop-extension/config.js
@@ -4,6 +4,6 @@
 // For production, consider using Chrome's storage API or a build tool
 
 const CONFIG = {
-    DEEPSEEK_API_KEY: 'sk-82c0ea79f27041378d0c9b9a9e056aee'
+    DEEPSEEK_API_KEY: 'sk-ef7709dcbad449cfbcb17d2f7700d112'
 };
 
