f05e5124a8f1bf00b906984728938954f6ae7949
SSOBHY2 <mohamedsobhy672005@gmail.com>
2025-11-17 21:00:09 -0500

    update config.js

---
+++ lexiloop-extension/config.js
@@ -0,0 +1,9 @@
+// Configuration file - API key loaded from .env
+// Note: In a browser extension, we can't directly read .env files at runtime
+// This file should be updated manually or via a build process
+// For production, consider using Chrome's storage API or a build tool
+
+const CONFIG = {
+    DEEPSEEK_API_KEY: 'sk-82c0ea79f27041378d0c9b9a9e056aee'
+};
+
